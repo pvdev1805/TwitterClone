c33362c83501f4ec2e6f7a6dab99346d096c74f4
Phu Vo <u1153426@umail.usq.edu.au>
2025-02-01 09:39:12 +1000

    🔨 Setup user login (route + middleware + controller)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import express from 'express'
-import userRouter from './routes/user.routes'
+import usersRouter from '~/routes/users.routes'
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
 
-app.use('/user', userRouter)
+app.use('/users', usersRouter)
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`)
