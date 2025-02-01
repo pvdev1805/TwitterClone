dfd8b72dc07333a7eaea16ae96c8f977d7eebb46
Phu Vo <u1153426@umail.usq.edu.au>
2025-02-02 00:25:28 +1000

    🔨 Implement User schema

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,15 +1,43 @@
 import { Request, Response } from 'express'
+import databaseService from '~/services/database.services'
+import usersService from '~/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
 
   if (email === 'pvdev1805@gmail.com' && password === '123123') {
-    return res.json({
+    res.json({
       message: 'Login successful'
     })
+
+    return
   }
 
-  return res.status(400).json({
+  res.status(400).json({
     error: 'Login failed'
   })
+  return
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  try {
+    const result = await usersService.register({ email, password })
+
+    // await databaseService.users.find({})
+
+    res.json({
+      message: 'Register successful',
+      result
+    })
+    return
+  } catch (error) {
+    res.status(400).json({
+      message: 'Register failed',
+      error
+    })
+
+    return
+  }
 }
